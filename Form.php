a116aab9cac8d411ea1a0fd61ea64cdc851910b8
iceand lee <804657038@qq.com>
2023-12-15 15:36:50 +0800

    2

--- Form.php
+++ Form.php
@@ -47,7 +47,11 @@ class Form
         if(isset($item['required'])){
             $required = "required";
         }
-        $html ='<select name="'.$item['field'].'" lay-filter="'.$item['field'].'" '.$required.'>';
+        $disabled = "";
+        if(isset($item['disabled']) && $item['disabled'] === true){
+            $disabled = "disabled";
+        }
+        $html ='<select name="'.$item['field'].'" '.$disabled.' lay-filter="'.$item['field'].'" '.$required.'>';
         foreach ($item['option'] as $val){
             if($item['value'] == $val['value']){
                 $html .='<option value="'.$val['value'].'" selected>'.$val['label'].'</option>';
@@ -73,13 +77,28 @@ class Form
         if(isset($item['required'])){
             $required = "required";
         }
-        $html ='<input '.$maxlenth.' name="'.$item['field'].'"  value="'.$item['value'].'" placeholder="请输入..." class="layui-input" '.$required.'>';
+        $disabled = "";
+        if(isset($item['disabled']) && $item['disabled'] === true){
+            $disabled = "disabled";
+        }
+        $html ='<input type="input" '.$maxlenth.' name="'.$item['field'].'"  '.$disabled.' value="'.$item['value'].'" placeholder="请输入..." class="layui-input" '.$required.'>';
 
         return $html;
     }
+    public function hidden($item){
+$html = <<<EOT
+<input type="hidden" name="{$item['field']}"  value="{$item['value']}" placeholder="请输入..." class="layui-input" >
+<script type="text/javascript">
+$('.{$item['field']}').hide()
+</script>
+EOT;
 
+
+
+        return $html;
+    }
     /**
-     * @title 数字
+     * @title 整数
      * @param ['type'=>'','label'=>'字段名称','field'=>'字段','value'=>'值','required'=>'是否必填','maxlenth'=>'字段长度']
      * @param $item
      * @return string
@@ -94,7 +113,37 @@ class Form
         if(isset($item['required'])){
             $required = "required";
         }
-        $html = '<input oninput="value=value.replace(/[^\d]/g,\'\')" type="number" '.$maxlenth.' '.$required.' name="'.$item['field'].'"  value="'.$item['value'].'" placeholder="请输入..." class="layui-input">';
+        $disabled = "";
+        if(isset($item['disabled']) && $item['disabled'] === true){
+            $disabled = "disabled";
+        }
+
+        $html = '<input oninput="value=value.replace(/[^\d]/g,\'\')" type="number" '.$maxlenth.' '.$disabled.' '.$required.' name="'.$item['field'].'"  value="'.$item['value'].'" placeholder="请输入..." class="layui-input">';
+        return $html;
+
+    }
+    /**
+     * @title 浮点数
+     * @param ['type'=>'','label'=>'字段名称','field'=>'字段','value'=>'值','required'=>'是否必填','maxlenth'=>'字段长度']
+     * @param $item
+     * @return string
+     */
+    public function float($item){
+        $maxlenth ="";
+        if(isset($item['maxlenth']) && abs($item['maxlenth'])>0){
+            $maxlenth = "maxlength='{$item['maxlenth']}'";
+        }
+
+        $required = "";
+        if(isset($item['required'])){
+            $required = "required";
+        }
+        $disabled = "";
+        if(isset($item['disabled']) && $item['disabled'] === true){
+            $disabled = "disabled";
+        }
+
+        $html = '<input oninput="value=value.replace(/[^-?([1-9]\d*\.\d*|0\.\d*[1-9]\d*|0?\.0+|0)]/g,\'\')" type="number" '.$maxlenth.' '.$disabled.' '.$required.' name="'.$item['field'].'"  value="'.$item['value'].'" placeholder="请输入..." class="layui-input">';
         return $html;
 
     }
@@ -113,7 +162,11 @@ class Form
         if(isset($item['required'])){
             $required = "required";
         }
-        $html ='<textarea '.$maxlenth.' name="'.$item['field'].'" '.$required.' class="layui-textarea" placeholder="请输入数据内容">'.$item['value'].'</textarea>';
+        $disabled = "";
+        if(isset($item['disabled']) && $item['disabled'] === true){
+            $disabled = "disabled";
+        }
+        $html ='<textarea '.$maxlenth.' name="'.$item['field'].'" '.$required.' '.$disabled.' class="layui-textarea" placeholder="请输入数据内容">'.$item['value'].'</textarea>';
         return $html;
     }
     /**
@@ -138,15 +191,23 @@ EOT;
      * @return string
      */
     public function img($item){
-$html = <<<EOT
-<input type="hidden" data-cut-width="500" data-cut-height="500" data-max-width="500" data-max-height="500" name="{$item['field']}" value="{$item['value']}">
+        if(isset($item['disabled']) && $item['disabled'] === true){
+            $html = <<<EOT
+<img src="{$item['value']}" width="80">
+EOT;
+
+        }else{
+            $html = <<<EOT
+<input type="hidden" data-quality="0.5" name="{$item['field']}" value="{$item['value']}">
 <script>$('[name={$item['field']}]').uploadOneImage();</script>
 EOT;
+        }
+
         return $html;
     }
 
     /**
-     * @title 时间选择插件
+     * @title 日期选择插件
      * @param $item
      */
     public function date($item){
@@ -157,6 +218,17 @@ EOT;
 
     }
 
+    /**
+     * @title 日期时间选择插件
+     * @param $item
+     * @return string
+     */
+    public function datetime($item){
+        $html = <<<EOT
+<input data-date-input="datetime" name="{$item['field']}" value="{$item['value']}" placeholder="请选择时间" class="layui-input">
+EOT;
+        return $html;
+    }
     /**
      * @title 时间范围旋转
      * @param $item
@@ -238,9 +310,147 @@ $html .= <<<EOT
 EOT;
         return $html;
     }
+    /**
+     * @title 三级联动搜索
+     * @param [
+     * 'type'=>'select',
+     * 'label'=>'字段名称',
+     * 'field'=>'字段',
+     * 'value'=>'值',
+     * 'required'=>'是否必填',
+     * 'option|第一个选项'=>[['value'=>'选项值','label'=>'选项名称']],
+     * 'param'=>[['field'=>'参数1字段','value'=>'参数1值'],['field'=>'参数2字段','value'=>'参数2值']]],
+     * 'ajaxlist'=>'ajax请求list'
+     * @param $item
+     */
+    public function linkage3($item){
+        $html = '<div class="layui-input-inline">';
 
+        $html .='<select name="'.$item['param'][0]['field'].'" lay-filter="'.$item['param'][0]['field'].'">';
+        $html .='<option value="">请选择</option>';
+        foreach ($item['option'] as $val){
+            if($item['param'][0]['value'] == $val['value']){
+                $html .='<option value="'.$val['value'].'" selected>'.$val['label'].'</option>';
+            }else{
+                $html .='<option value="'.$val['value'].'">'.$val['label'].'</option>';
+            }
+        }
+        $html .='</select>';
 
+        $html .='</div>';
+
+        $html .= '<div class="layui-input-inline">';
+        $html .='<select name="'.$item['param'][1]['field'].'" lay-filter="'.$item['param'][1]['field'].'">';
+        $html .='<option value="">请选择</option>';
+        $html .='</select>';
+
+        $html .='</div>';
+
+        $html .= '<div class="layui-input-inline">';
+        $html .='<select name="'.$item['param'][2]['field'].'" lay-filter="'.$item['param'][2]['field'].'">';
+        $html .='<option value="">请选择</option>';
+        $html .='</select>';
+
+        $html .='</div>';
+
+        $html .= <<<EOT
+<script>
+        function ajaxlist{$item['field']}(pid,parent) {
+            
+             $.form.load('{$item['ajaxurl']}', {pid:pid}, "get", function(ret) {
+               var field1 = '{$item['param'][1]['value']}'
+               var field2 = '{$item['param'][2]['value']}'
+               var field_value = 0;
+               if(parent == 1){
+                 field_value = field1
+               }else{
+                 field_value = field2  
+               }
+                     var h ='<option value="">请选择</option>'
+                     ret.info.forEach(function(val){
+                         if(field_value == val.value){
+                             h +='<option value="'+val.value+'" selected>'+val.label+'</option>'
+                         }else{
+                             h +='<option value="'+val.value+'">'+val.label+'</option>'
+                         }
+                     })
+                     if(parent == 1){
+                          $('[name="{$item['param'][1]['field']}"]').html(h)
+                          if(field_value){
+                               ajaxlist{$item['field']}(field_value,2)
+                          }  
+                     }else{
+                         $('[name="{$item['param'][2]['field']}"]').html(h)
+                     }
+                     layui.form.render('select');
+                     return false
+             }, null, null, 'false');
+        }
+        $(function() {
+           // 数据状态切换操作
+            layui.form.on('select({$item['param'][0]['field']})', function (obj) {
+   
+                ajaxlist{$item['field']}(obj.value,1)
+            
+            });
+            layui.form.on('select({$item['param'][1]['field']})', function (obj) {
+   
+                ajaxlist{$item['field']}(obj.value,2)
+            
+            });
+            if($('[name="{$item['param'][0]['field']}"]').val()){
+                ajaxlist{$item['field']}($('[name="{$item['param'][0]['field']}"]').val(),1)
+            }
+            
+        })
+        
+</script>         
+EOT;
+        return $html;
+    }
+
+    /**
+     * @title 行input输入
+     * @param $item
+     * @return string
+     */
+    public function inlineInput($item){
+        $html = '';
+        $disabled = "";
+        if(isset($item['disabled']) && $item['disabled'] === true){
+            $disabled = "disabled";
+        }
+        $required = "";
+        if(isset($item['required'])){
+            $required = "required";
+        }
+        foreach ($item['option'] as $val){
+            $html .='<div class="layui-input-inline">';
+            $html .=$val['title'];
+            $script = "";
+            if(isset($val['script'])){
+                $script = 'oninput="'.$val['script'].'"';
+            }
+
+            $type = 'type="text"';
+            if(isset($val['type'])){
+                $type = 'type="number"';
+            }
+            $maxlenth ="";
+            if(isset($val['maxlenth']) && abs($val['maxlenth'])>0){
+                $maxlenth = "maxlength='{$val['maxlenth']}'";
+            }
+            $placeholder = "请输入...";
+            if(isset($val['placeholder'])){
+                $placeholder = $val['placeholder'];
+            }
+            $html .= '<input '.$script.' '.$type.' '.$maxlenth.' '.$disabled.' '.$required.' name="'.$val['field'].'"  value="'.$val['value'].'" placeholder="'.$placeholder.'" class="layui-input">';
+            $html .='</div>';
+        }
+        return $html;
+    }
     public static function template($field = [],$formType=""){
+
         foreach ($field as &$val){
             if(method_exists((new self),$val['type'])){
                 $method = $val['type'];
@@ -264,9 +474,9 @@ EOT;
      * @param $data
      * @return array
      */
-    public static function initForm($data):array {
+    public static function initForm($data,$value=[]):array {
         $resData = [];
-        $notSite = ['type','label','field','value','required'];
+        $notSite = ['type','label','field','value','required','class_name'];
 
         foreach ($data as $val){
             $vars = get_object_vars($val);
@@ -277,6 +487,11 @@ EOT;
                 }else{
                     if($v) $item[$k]=$v;
 
+                }
+            }
+            if(!$item['value']){
+                if(isset($value[$item['field']])){
+                    $item['value'] = $value[$item['field']];
                 }
             }
             $resData[]=$item;
--- FormItem.php
+++ FormItem.php
@@ -12,16 +12,23 @@ namespace iceand;
  * Class FormItem
  * @package iceand
  * * 静态助手调用
+ * @method static FormItem hidden(string $field, string $label) 隐藏输入
  * @method static FormItem input(string $field, string $label) 单行输入
  * @method static FormItem number(string $field, string $label) 数字输入
+ * @method static FormItem float(string $field, string $label) 浮点数输入
  * @method static FormItem radio(string $field, string $label) 单选
+ * @method static FormItem checkbox(string $field, string $label) 多选
  * @method static FormItem select(string $field, string $label) 下拉选择
  * @method static FormItem textarea(string $field, string $label) 多行输入
  * @method static FormItem img(string $field, string $label) 单图上传
  * @method static FormItem linkage2(string $field, string $label) 二级联动select
+ * @method static FormItem linkage3(string $field, string $label) 三级联动select
  * @method static FormItem editor(string $field, string $label) 富文本编辑器
  * @method static FormItem date(string $field, string $label) 日期选择
+ * @method static FormItem datetime(string $field, string $label) 日期时间选择
  * @method static FormItem dateRange(string $field, string $label) 日期范围选择
+ * @method static FormItem legend(string $field, string $label) 字块集
+ * @method static FormItem inlineInput(string $field, string $label) 一行多个输入框
  */
 class FormItem
 {
@@ -35,6 +42,11 @@ class FormItem
     public $maxlenth;
     public $param;
     public $ajaxurl;
+    public $legend;
+    public $disabled;
+    public $class_name="";
+    public $hide = false;
+    public $script;
     /**
      * 静态魔术方法
      * @param string $method 方法名称
@@ -47,6 +59,7 @@ class FormItem
         $self->type = $method;
         $self->field = $args[0];
         $self->label = $args[1];
+
         return $self;
     }
 
@@ -59,4 +72,42 @@ class FormItem
     public function maxlenth($maxlenth){$this->maxlenth = $maxlenth;return $this;}
     public function ajaxUrl($url){$this->ajaxurl = $url;return $this;}
     public function param($val){$this->param = $val;return $this;}
+    public function disabled(bool $val){$this->disabled = $val;return $this;}
+    public function className(string $val){$this->class_name = $val;return $this;}
+    public function hide(){$this->hide = true;return $this;}
+    public function showhide(array $val,$common=''){
+        $tyle_field = "";
+        switch ($this->type){
+            case "radio":
+                $tyle_field = "radio({$this->field})";
+                break;
+            case "select":
+                $tyle_field = "select({$this->field})";
+                break;
+            case "checkbox":
+                $tyle_field = "checkbox({$this->field})";
+                break;
+        }
+        $json = json_encode($val,256);
+        if($this->type == "radio"){
+            $html = <<<EOT
+<script>
+var json = JSON.parse('{$json}')
+$('.{$this->field}').on('change',function(){
+    var val = $('[name="{$this->field}"]:checked').val()
+    console.log("cal",val)
+    $('.{$common}').hide()
+     var clsn =  json[val]     
+     $('.'+clsn).show()
+
+})
+
+</script>
+EOT;
+
+        }
+
+
+        $this->script = $html;return $this;
+    }
 }
