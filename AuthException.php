d65833d70fca21da86a616e0c72e02871e4dbff6
iceand lee <804657038@qq.com>
2024-01-03 11:35:34 +0800

    2

---
+++ AuthException.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: huawei
+ * Date: 2022/12/11
+ * Time: 21:36
+ */
+
+
+use think\exception\HttpResponseException;
+
+class AuthException extends HttpResponseException
+{
+    public function __construct($message, $code = 0)
+    {
+
+        parent::__construct(json()->data([
+            "code"=>$code,
+            "info"=>$message,
+            "data"=>[],
+            "url"=>""
+        ]));
+
+    }
+}
